66cbfc207ee70844b977a20dbde1cf10de2370a8
sachbij06 <sach.bijlani15@gmail.com>
2024-09-10 14:15:05 -0400

    test

--- Kicking Code/website/to-do.py
+++ Kicking Code/website/to-do.py
@@ -1,7 +1,6 @@
 #to-do
 
 
-# 1. Adjust math inside the session.py charting handler (coordinates are a little off with college posts)
 # 2. Create a better UI for the homepage
 # 3. Sort through stats in _math.py and clean up the handling
 # 4. Setup the visual that you wrote down on the piece of paper inside the stats.html file
