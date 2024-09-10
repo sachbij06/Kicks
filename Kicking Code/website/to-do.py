8faac9a44de466c61737389bfbb12c7ed31f781a
sachb <sach.bijlani15@gmail.com>
2024-09-10 13:00:04 -0400

    to-do

---
+++ Kicking Code/website/to-do.py
@@ -0,0 +1,9 @@
+#to-do
+
+
+# 1. Adjust math inside the session.py charting handler (coordinates are a little off with college posts)
+# 2. Create a better UI for the homepage
+# 3. Sort through stats in _math.py and clean up the handling
+# 4. Setup the visual that you wrote down on the piece of paper inside the stats.html file
+# 5. Create KO charting capability in session.html/session.py
+# 6. Think about how to enhance Visualization page (do we even need it if we have the statistics page?)
