c5f3113ba7330eefe762cea2c2cfe335469835c9
sachbij06 <sach.bijlani15@gmail.com>
2024-09-23 02:36:39 -0400

    separated code for displaying all session kicks and session kicks

--- Kicking Code/website/to-do.py
+++
@@ -1,6 +0,0 @@
-#to-do
-
-# 3. Sort through stats in _math.py and clean up the handling
-# 4. Setup the visual that you wrote down on the piece of paper inside the stats.html file
-# 5. Create KO charting capability in session.html/session.py
-# 6. Think about how to enhance Visualization page (do we even need it if we have the statistics page?)
